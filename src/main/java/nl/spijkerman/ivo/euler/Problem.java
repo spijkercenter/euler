a7c33551121db8701e267cbcb86d308285875178
Ivo Spijkerman <spijkerman@qcast.nl>
2019-05-23 14:47:38 +0200

    added p2

--- src/main/java/nl/spijkerman/ivo/euler/Problem.java
+++ src/main/java/nl/spijkerman/ivo/euler/Problem.java
@@ -1,24 +1,32 @@
 package nl.spijkerman.ivo.euler;
 
-import com.sun.istack.internal.NotNull;
 
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
 import java.time.Instant;
 
-public interface Problem {
-    @NotNull int run();
+interface Problem {
 
-    @NotNull String description();
+    @NotNull
+    String run();
 
-    public static void main(String[] args) {
+    @NotNull
+    String description();
+
+    static void main(String[] args) {
 
         Instant begin = Instant.now();
-        Problem problem = new Problem1();
+        Problem problem = new Problem2();
         System.out.println("Running " + problem.getClass().getSimpleName());
         System.out.println("\n" + problem.description());
         System.out.println("\nSolution = " + problem.run());
 
         Instant now = Instant.now();
-
+        long millis = Duration.between(begin, now).toMillis();
+        System.out.printf("Solved in %ds%dms%n", millis / 1000, millis % 1000);
 
     }
 }
--- src/main/java/nl/spijkerman/ivo/euler/Problem1.java
+++ src/main/java/nl/spijkerman/ivo/euler/Problem1.java
@@ -1,23 +1,29 @@
 package nl.spijkerman.ivo.euler;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.stream.IntStream;
 
 /**
  * https://projecteuler.net/problem=1
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23.
- *
+ * <p>
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 public class Problem1 implements Problem {
 
-    public int run() {
+    @NotNull
+    @Override
+    public String run() {
         return IntStream.range(3, 1000)
                 .filter(i -> i % 3 == 0 || i % 5 == 0)
-                .sum();
+                .sum() + "";
     }
 
 
+    @NotNull
+    @Override
     public String description() {
         return "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\n\nFind the sum of all the multiples of 3 or 5 below 1000.";
     }
